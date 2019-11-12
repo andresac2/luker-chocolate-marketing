4230edd35325c3e67b4cb2c0f2811bbba9b6cfa2
stiventc <stivent367@gmail.com>
2019-11-12 03:42:23 -0500

    Create Our Aroma component

---
+++ src/components/our-value/aroma/our-aroma.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../assets/img/LukerlogoDark.svg'
+import altImg from '../../assets/img/img-example.svg'
+
+const OurAroma = () => {
+
+  return (
+    <div className="our-aroma-component" >
+      <h1>lalala sdvjbsdfjlbsdgkj</h1>
+    </div >
+  )
+}
+
+export default OurAroma;
