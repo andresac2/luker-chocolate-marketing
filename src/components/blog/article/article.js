c41dcb1125525137a60efffc99d6ad7f81abfc2d
stiventc <stivent367@gmail.com>
2019-11-19 14:11:55 -0500

    feat article blog

--- src/components/blog/article/article.js
+++ src/components/blog/article/article.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Breadcrumb, Icon } from 'antd';
+import { Link } from 'react-router-dom';
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaPrint } from 'react-icons/fa';
+
+import plant from '../../../assets/img/planting.png'
+import autorPic from '../../../assets/img/autor-blog.png'
+
 
 function Article() {
   return (
@@ -26,9 +31,43 @@ function Article() {
           <a href="" target="_blank" ><FaPrint /></a>
         </div>
       </div>
-      <div className="blog-article-content">ezsxdrctfvygbuhnijmok,</div>
-      <div className="blog-article-autor"></div>
-      <div className="blog-article-entries"></div>
+      <div className="blog-article-content">
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <div className="blog-article-content--img">
+          <img src={plant} alt="Proceso de plantación" />
+          <span>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</span>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="blog-article-autor">
+        <img src={autorPic} alt="Autor" />
+        <div className="blog-article-autor--data">
+          <h2>NOMBRE APELLIDO</h2>
+          <span>DETALLES PROFESIONALES</span>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.</p>
+          <a href="https://www.linkedin.com/company/casa-luker/" target="_blank" ><FaLinkedinIn /> Nombre del perfil</a>
+        </div>
+      </div>
+      <div className="blog-article-entries">
+        <h2>RECOMMENDED ENTRIES</h2>
+        <div className="blog-article-entries--list">
+          <Link to="/blog/take-stand/article">
+            <img src={plant} alt="cacao" />
+            <p>DETALLES DE PUBLICACIÓN</p>
+            <h2>Lorem Ipsum</h2>
+          </Link>
+          <Link to="/blog/take-stand/article">
+            <img src={plant} alt="cacao" />
+            <p>DETALLES DE PUBLICACIÓN</p>
+            <h2>Lorem Ipsum</h2>
+          </Link>
+          <Link to="/blog/take-stand/article">
+            <img src={plant} alt="cacao" />
+            <p>DETALLES DE PUBLICACIÓN</p>
+            <h2>Lorem Ipsum</h2>
+          </Link>
+        </div>
+      </div>
       <div className="blog-article-comments"></div>
     </div>
   );
