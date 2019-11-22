6783ad1d9ab17809db892c3880f8494faad2c034
stiventc <stivent367@gmail.com>
2019-11-22 13:52:21 -0500

    Added solution

--- src/scenes/Solution/Solution.js
+++ src/scenes/Solution/Solution.js
@@ -7,24 +7,25 @@ function Solution() {
 
   return (
     <div className='solution-component'>
-      <div className="solution-component--service">
-        <Link to="/services/maquila" className="header-btn">
-          SERVICIOS
-        </Link>
+
+      <div className="solution-component--ingredient">
         <div className="btn-dist">
           <img src={logo} className="logo" alt="Logo Luker" />
           <button onClick={() => console.log('hi')}> DISTRIBUIDORES </button>
         </div>
+        <Link to="/services/ingredients" className="header-btn">
+          INGREDIENTS
+        </Link>
       </div>
       <div className="solution-component--maquila">
         <Link to="/services/maquila" className="header-btn">
-          MAQUILA
+          FINISHED CHOCOLATE PRODUCTS
             </Link>
       </div>
-      <div className="solution-component--ingredient">
-        <Link to="/services/ingredients" className="header-btn">
-          INGREDIENTES
-            </Link>
+      <div className="solution-component--service">
+        <Link to="/services/maquila" className="header-btn">
+          OUR SERVICES
+        </Link>
       </div>
     </div>
   );
