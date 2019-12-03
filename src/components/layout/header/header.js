3c79695d5cabb4284875befa0c816911e4dba3a7
stiventc <stivent367@gmail.com>
2019-12-03 04:14:21 -0500

    products responsive

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -38,7 +38,7 @@ class Header extends React.Component {
         <div className={`header-component-responsive header-component-responsive-${showMenu && 'visible'} header-component-responsive-${selectTab.slice(1).split('/').shift()}`} onClick={() => this.menuToggle()} >
           <div className={`header-logo header-logo-${showMenu && 'visible'}`}>
             <img src={logo} className="logo" alt="Logo Luker" />
-            {!showMenu && <Select defaultValue="es" >
+            {showMenu && <Select defaultValue="es" >
               <Option value="es">ES</Option>
               <Option value="en">EN</Option>
             </Select>}
