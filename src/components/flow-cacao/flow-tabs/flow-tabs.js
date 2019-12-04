b0f69c2b878423919e3f6d0d4071325ec58ce86a
stiventc <stivent367@gmail.com>
2019-12-04 16:56:38 -0500

    Style(Flow): Adjust font-size in tabs

--- src/components/flow-cacao/flow-tabs/flow-tabs.js
+++ src/components/flow-cacao/flow-tabs/flow-tabs.js
@@ -108,11 +108,11 @@ class FlowTabs extends React.Component {
           </div>
         </div>
         <div className={`flow-tab flow-tab--${selectTab}`}>
-          <div onClick={() => this.tabToggle('cacao')} className={`flow-tab-item flow-tab-item--cacao flow-tab-item--${selectTab === 'cacao' && 'active'}`}><span>CACAO FINO DE AROMA</span></div>
+          <div onClick={() => this.tabToggle('cacao')} className={`flow-tab-item flow-tab-item--cacao flow-tab-item--${selectTab === 'cacao' && 'active'}`}><span>CACAO FINO DE <br></br> AROMA</span></div>
           <div onClick={() => this.tabToggle('granjeros')} className={`flow-tab-item flow-tab-item--granjeros flow-tab-item--${selectTab === 'granjeros' && 'active'}`}><span>HARVESTING</span></div>
           <div onClick={() => this.tabToggle('abastecimiento')} className={`flow-tab-item flow-tab-item--abastecimiento flow-tab-item--${selectTab === 'abastecimiento' && 'active'}`}><span>POST-HARVESTING</span></div>
           <div onClick={() => this.tabToggle('procesamiento')} className={`flow-tab-item flow-tab-item--procesamiento flow-tab-item--${selectTab === 'procesamiento' && 'active'}`}><span>PROCESSING</span></div>
-          <div onClick={() => this.tabToggle('coberturas')} className={`flow-tab-item flow-tab-item--coberturas flow-tab-item--${selectTab === 'coberturas' && 'active'}`}><span>REFINEMENT AND CONCHING</span></div>
+          <div onClick={() => this.tabToggle('coberturas')} className={`flow-tab-item flow-tab-item--coberturas flow-tab-item--${selectTab === 'coberturas' && 'active'}`}><span>REFINEMENT AND <br></br> CONCHING</span></div>
           <div onClick={() => this.tabToggle('producto')} className={`flow-tab-item flow-tab-item--producto flow-tab-item--${selectTab === 'producto' && 'active'}`}><span>FINISHED PRODUCTS </span></div>
         </div>
         <div></div>
