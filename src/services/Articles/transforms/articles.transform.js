fd00fec53bdaa3fda3eb39f9f115c90688fea775
Yeisom Gomez <yeisom40@gmail.com>
2020-05-01 19:52:53 -0500

    refactor(blog): sort artciles in spanish

--- src/services/Articles/transforms/articles.transform.js
+++ src/services/Articles/transforms/articles.transform.js
@@ -13,6 +13,21 @@ const months = {
   DECEMBER: 12
 }
 
+const monthsES = {
+  ENERO: 1,
+  FEBRERO: 2,
+  MARZO: 3,
+  ABRIL: 4,
+  MAYO: 5,
+  JUNIO: 6,
+  JULIO: 7,
+  AGOSTO: 8,
+  SEPTIEMBRE: 9,
+  OCTUBRE: 10,
+  NOVIEMBRE: 11,
+  DICIEMBRE: 12
+}
+
 export default (articles) => {
   return articles.map((data, i) => {
     let art = {}
@@ -24,7 +39,7 @@ export default (articles) => {
     art['date'] = data.acf.date;
 
     const [ month, year ] = data.acf.date.split(' ');
-    art._date = new Date(year + '-' + months[month] + '-02');
+    art._date = new Date(year + '-' + (months[month] || monthsES[month]) + '-02');
 
     art['content'] = data.content.rendered;
     art.autor = {
