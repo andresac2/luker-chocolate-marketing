e0176ec031730199e1063e913d24723ab3f1d1c5
Yeisom Gomez <yeisom40@gmail.com>
2020-05-05 19:13:02 -0500

    test sitemap hook

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 app.post('/sitemap-hook', async (req, res, next) => {
   const { sitemap } = req.body
-  const sitemapEdit = await replaceXml(sitemap)
+  //const sitemapEdit = await replaceXml(sitemap)
   res.send("ENTRO") 
 });
 
