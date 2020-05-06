caeb9ca940b6fa3650b3ec9df49d9d459dfba578
Yeisom Gomez <yeisom40@gmail.com>
2020-05-05 20:35:34 -0500

    refactor(): index delete getTranslation server

--- server/index.js
+++ server/index.js
@@ -11,9 +11,6 @@ const app = express();
 const router = express.Router();
 var fs = require('fs');
 
-getTranslations('es')
-getTranslations('en')
-
 router.use(express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -33,9 +30,8 @@ router.use('*', renderer);
 app.use(router);
 
 app.listen(process.env.PORT || PORT, '0.0.0.0', (error) => {
-  if (error) {
+  if (error) 
     return console.log('something bad happened', error);
-  }
 
   console.log("listening on " + PORT + "...");
 });
@@ -48,23 +44,5 @@ const replaceXml = (sitemap) => {
       else
         resolve({ success: 'OK' })
     });
-  })
-}
-
-function getTranslations(lng) {
-  return new Promise((resolve) => {
-    let pages = [{}];
-    fetch(`https://www.back.lukerchocolate.com${lng === 'en' ? '' : ('/' + lng)}/wp-json/wp/v2/pages?per_page=100`)
-      .then((response) => response.json())
-      .then((response) => {
-        response.map((data, i) => pages.push(pages[0][data.slug] = data.acf))
-
-        fs.writeFile(`src/locales/${lng}/translation.json`, JSON.stringify(pages[0]), 'utf8', (err) => {
-          if (err)
-            throw err
-          else
-            resolve({ success: 'OK' })
-        });
-      });
   })
 }
