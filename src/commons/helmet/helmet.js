75866458c62a3670f7aff9c7db6f95651adabce2
Yeisom Gomez <yeisom40@gmail.com>
2020-06-08 09:00:29 -0500

    fix(home): seo helmet name image

--- src/commons/helmet/helmet.js
+++ src/commons/helmet/helmet.js
@@ -12,9 +12,9 @@ class HelmetComponent extends Component {
         <meta name="description" content={description ? description : 'More than a century of experience has given us what it takes to provide high-quality chocolate products and services around the world with Cacao Fino de Aroma. Sustainability and innovation are key to our process. Get to know us!'} />
         <meta property="og:title" content={title ? titleOg ? titleOg : title.charAt(0).toUpperCase() + title.slice(1) : "Luker Chocolate | Cacao Fino de Aroma"} />
         <meta property="og:description" content={description ? descriptionOg ? descriptionOg : description : "More than a century of experience has given us what it takes to provide high-quality chocolate products and services around the world with Cacao Fino de Aroma. Sustainability and innovation are key to our process. Get to know us!"} />
-        <meta name="keywords" content={keywords ? keywords : "cacao fino de aroma, high-quality chocolate products, chocolate products and services, chocolate"} />
-        <meta property="og:image" content={cover ? cover : "https://www.back.lukerchocolate.com/wp-content/uploads/2020/01/Home.jpg"} />
+        <meta name="image" property="og:image" content={cover ? cover : "https://www.back.lukerchocolate.com/wp-content/uploads/2020/01/Home.jpg"} />
         <meta property="og:url" content={fullUrl} />
+        <meta name="keywords" content={keywords ? keywords : "cacao fino de aroma, high-quality chocolate products, chocolate products and services, chocolate"} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="og:site_name" content="Luker Chocolate." />
         <meta name="twitter:image:alt" content={description ? descriptionOg ? descriptionOg : description : "More than a century of experience has given us what it takes to provide high-quality chocolate products and services around the world with Cacao Fino de Aroma. Sustainability and innovation are key to our process. Get to know us!"} />
