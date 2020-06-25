56e5d0662e0ce293288e840ee6939076154b4336
Yeisom Gomez <yeisom40@gmail.com>
2020-06-25 12:06:10 -0500

    fix(article): parser article in server

--- server/template.js
+++ server/template.js
@@ -24,7 +24,7 @@ export async function parseArticle(articles, url, articlesEs) {
 		breads[1]['name'] = e.acf.categoria?.name;
 		art['autor'] = autor;
 		art['breads'] = breads;
-		art.article_equivalent = data.acf.article_equivalent
+		art.article_equivalent = e.acf.article_equivalent
 		arts.push(art);
 		art = {};
 		breads = [{}, {}];
@@ -49,7 +49,7 @@ export async function parseArticle(articles, url, articlesEs) {
 		breads[1]['name'] = e.acf.categoria?.name;
 		art['autor'] = autor;
 		art['breads'] = breads;
-		art.article_equivalent = data.acf.article_equivalent
+		art.article_equivalent = e.acf.article_equivalent
 		arts.push(art);
 		art = {};
 		breads = [{}, {}];
